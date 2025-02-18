aca130f4c1d0d4a4d7a51a689c331f269e3754b8
mlamp99 <michael.lamp@avnet.com>
2025-02-18 16:56:52 -0600

    Update install.sh
    Signed-off-by: mlamp99 <michael.lamp@avnet.com>

--- nxp-frdm-imx-93/dms-demo/scripts/install.sh
+++ nxp-frdm-imx-93/dms-demo/scripts/install.sh
@@ -3,17 +3,29 @@
 echo "Updating environment variables..."
 export PATH=$PATH:/usr/local/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-export PIP_ROOT_USER_ACTION=ignore  # Suppresses venv warning
 
 echo "Upgrading Pip..."
-python3 -m pip install --upgrade pip  
+PIP_ROOT_USER_ACTION=ignore python3 -m pip install --upgrade pip  
 
 echo "Installing dependencies..."
-pip install --upgrade pip  
-pip install flask numpy opencv-python requests iotconnect-sdk-lite
+PIP_ROOT_USER_ACTION=ignore pip install --upgrade pip  
+PIP_ROOT_USER_ACTION=ignore pip install flask numpy opencv-python requests iotconnect-sdk-lite
 
 echo "Installing IoTConnect SDK..."
-pip install iotconnect-sdk-lite  
+PIP_ROOT_USER_ACTION=ignore pip install iotconnect-sdk-lite  
 
 echo "Generating SSL certificates..."
-openssl req -x509 -nodes -days 365 -newkey rsa:2048
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /home/weston/key.pem -out /home/weston/cert.pem -subj "/CN=localhost"
+
+echo "Downloading IoTConnect Quickstart script..."
+cd /home/weston/
+curl -sSLo imx93-ai-demo.py "https://raw.githubusercontent.com/avnet-iotconnect/iotc-python-lite-sdk-demos/mcl-DMS-updates/nxp-frdm-imx-93/dms-demo/imx93-ai-demo.py"
+chmod +x imx93-ai-demo.py
+
+echo "Downloading DMS processing script..."
+mkdir -p /usr/bin/eiq-examples-git/dms
+curl -sSLo /usr/bin/eiq-examples-git/dms/dms-processing.py "https://raw.githubusercontent.com/avnet-iotconnect/iotc-python-lite-sdk-demos/mcl-DMS-updates/nxp-frdm-imx-93/dms-demo/dms-processing.py"
+chmod +x /usr/bin/eiq-examples-git/dms/dms-processing.py
+
+echo "Installation complete! You can now run the IoTConnect script:"
+echo "python3 /home/weston/imx93-ai-demo.py"
