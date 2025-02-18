21d1902e9b085585c076a4264a10a39f9281031d
mlamp99 <michael.lamp@avnet.com>
2025-02-18 15:55:19 -0600

    Update install.sh
    Signed-off-by: mlamp99 <michael.lamp@avnet.com>

--- nxp-frdm-imx-93/dms-demo/scripts/install.sh
+++ nxp-frdm-imx-93/dms-demo/scripts/install.sh
@@ -8,7 +8,7 @@ echo "Creating Python virtual environment..."
 python3 -m venv ~/iotconnect_env
 source ~/iotconnect_env/bin/activate
 pip install --upgrade pip
-pip install flask numpy opencv-python requests fcntl json avnet-iotconnect-sdk-lite
+pip install flask numpy opencv-python requests json avnet-iotconnect-sdk-lite
 
 echo "Installing IoTConnect SDK..."
 pip install iotconnect-sdk-lite
@@ -18,12 +18,12 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /home/weston/key.pem
 
 echo "Downloading IoTConnect Quickstart script..."
 cd /home/weston/
-wget -O imx93-ai-demo.py "https://raw.githubusercontent.com/avnet-iotconnect/iotc-python-lite-sdk-demos/mcl-DMS-updates/nxp-frdm-imx-93/dms-demo/imx93-ai-demo.py"
+curl -sSLo imx93-ai-demo.py "https://raw.githubusercontent.com/avnet-iotconnect/iotc-python-lite-sdk-demos/mcl-DMS-updates/nxp-frdm-imx-93/dms-demo/imx93-ai-demo.py"
 chmod +x imx93-ai-demo.py
 
 echo "Downloading DMS processing script..."
 mkdir -p /usr/bin/eiq-examples-git/dms
-wget -O /usr/bin/eiq-examples-git/dms/dms-processing.py "https://raw.githubusercontent.com/avnet-iotconnect/iotc-python-lite-sdk-demos/mcl-DMS-updates/nxp-frdm-imx-93/dms-demo/dms-processing.py"
+curl -sSLo /usr/bin/eiq-examples-git/dms/dms-processing.py "https://raw.githubusercontent.com/avnet-iotconnect/iotc-python-lite-sdk-demos/mcl-DMS-updates/nxp-frdm-imx-93/dms-demo/dms-processing.py"
 chmod +x /usr/bin/eiq-examples-git/dms/dms-processing.py
 
 echo "Installation complete! You can now run the IoTConnect script:"
