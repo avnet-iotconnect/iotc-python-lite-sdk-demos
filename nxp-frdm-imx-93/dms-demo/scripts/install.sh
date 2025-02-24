7591acee7975559fcdb14b0f4ab98b4c860d16d6
mlamp99 <michael.lamp@avnet.com>
2025-02-24 11:10:01 -0600

    Update install.sh
    Signed-off-by: mlamp99 <michael.lamp@avnet.com>

--- nxp-frdm-imx-93/dms-demo/scripts/install.sh
+++ nxp-frdm-imx-93/dms-demo/scripts/install.sh
@@ -98,7 +98,7 @@ if [[ "$model_choice" == "y" || "$model_choice" == "Y" ]]; then
     }
     chmod +x /usr/bin/eiq-examples-git/download_models.py 
     echo "Downloading eIQ AI Models..."
-    if python3 download_models.py 2>/dev/null; then
+    if python3 /usr/bin/eiq-examples-git/download_models.py 2>/dev/null; then
         echo "eIQ AI Models downloaded successfully."
     else
         echo "Warning: There was an error downloading eIQ AI Models. Please verify the model URLs and file formats."
