815bfe688e61ae23bdc1361dcced6ce7b3841e64
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-02-26 14:13:04 -0700

    Add files via upload
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

---
+++ nxp-frdm-imx-93/dms-demo/scripts/ota_install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define the target directories
+target_dir_dms="/usr/bin/eiq-examples-git/dms"
+target_dir_tflite="/usr/bin/eiq-examples-git/models"
+
+# Loop through each file in the current directory
+for file in *; do
+  # Check if it's a file (not a directory)
+  if [ -f "$file" ]; then
+    case "$file" in
+      "dms-processing.py")
+        # Move dms-processing to /usr/bin/eiq-examples-git/dms
+        mv "$file" "$target_dir_dms"
+        echo "Moved $file to $target_dir_dms"
+        ;;
+      *.tflite)
+        # Move .tflite files to /usr/bin/eiq-examples-git/models
+        mv "$file" "$target_dir_tflite"
+        echo "Moved $file to $target_dir_tflite"
+        ;;
+      *)
+        # If the file doesn't match any condition, do nothing
+        ;;
+    esac
+  fi
+done
