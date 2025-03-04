764e38ff87e016ca6e0724431cb6f93c828a1f49
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-03-04 12:38:49 -0700

    Create package_creation.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

---
+++ nxp-frdm-imx-93/dms-demo/scripts/package_creation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define the current directory and the directories for the other files
+CURRENT_DIR=$(pwd)
+PARENT_DIR=$(dirname "$CURRENT_DIR")
+ADDITIONAL_MODELS_DIR="$PARENT_DIR/additional_models"
+
+# Define the output tar.gz file name
+OUTPUT_PACKAGE="package_creation.tar.gz"
+
+# Files to include in the package
+FILES_TO_INCLUDE=(
+    "$CURRENT_DIR/ota_install.sh"
+    "$PARENT_DIR/dms-processing.py"
+    "$PARENT_DIR/imx93-ai-demo.py"
+    "$PARENT_DIR/download_models.py"
+)
+
+# Find all .tflite files in the additional_models directory
+TFLITE_FILES=$(find "$ADDITIONAL_MODELS_DIR" -name "*.tflite")
+
+# Create the tar.gz package
+tar -czvf "$OUTPUT_PACKAGE" "${FILES_TO_INCLUDE[@]}" $TFLITE_FILES
+
+# Confirm the creation of the package
+echo "Package $OUTPUT_PACKAGE created successfully."
