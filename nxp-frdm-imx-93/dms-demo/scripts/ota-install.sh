aee536cafe2f6bbf9c1518a84ba2ca3645e919e6
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-03-06 13:32:30 -0700

    Update ota-install.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- nxp-frdm-imx-93/dms-demo/scripts/ota-install.sh
+++ nxp-frdm-imx-93/dms-demo/scripts/ota-install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Define the target directories
-target_dir_download_models="/usr/bin/eiq-examples-git"
-target_dir_dms="/usr/bin/eiq-examples-git/dms"
 target_dir_tflite="/usr/bin/eiq-examples-git/models"
 
 # Loop through each file in the current directory
@@ -10,16 +8,6 @@ for file in *; do
   # Check if it's a file (not a directory)
   if [ -f "$file" ]; then
     case "$file" in
-      "download_models.py")
-        # Move download_models.py to /usr/bin/eiq-examples-git
-        mv "$file" "$target_dir_download_models"
-        echo "Moved $file to $target_dir_download_models"
-        ;;
-      "dms-processing.py")
-        # Move dms-processing to /usr/bin/eiq-examples-git/dms
-        mv "$file" "$target_dir_dms"
-        echo "Moved $file to $target_dir_dms"
-        ;;
       *.tflite)
         # Move .tflite files to /usr/bin/eiq-examples-git/models
         mv "$file" "$target_dir_tflite"
