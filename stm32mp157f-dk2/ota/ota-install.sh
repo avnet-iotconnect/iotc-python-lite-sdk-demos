4a421588afffca897861735448aeddf19fd263ff
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-03-18 12:58:43 -0600

    Update ota-install.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- stm32mp157f-dk2/ota/ota-install.sh
+++ stm32mp157f-dk2/ota/ota-install.sh
@@ -3,3 +3,6 @@
 # ---------UN-COMMENT THIS COMMAND TO ENABLE SDK RE-INSTALLATION-------
 # python3 -m pip install --force-reinstall iotconnect-sdk-lite
 # ---------------------------------------------------------------------
+
+# This is the most basic form of ota-install.sh where there are no files that need to be moved to additional directories. 
+# All files are extracted into /home/weston.
