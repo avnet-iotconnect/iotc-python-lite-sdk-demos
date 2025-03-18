1e044c6ce1b11ebaad5539153caa7c8ff10a109b
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-03-18 12:56:43 -0600

    Create ota-install.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

---
+++ stm32mp157f-dk2/ota/ota-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# ---------UN-COMMENT THIS COMMAND TO ENABLE SDK RE-INSTALLATION-------
+# python3 -m pip install --force-reinstall iotconnect-sdk-lite
+# ---------------------------------------------------------------------
