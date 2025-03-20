bded1092bc443da8baa6bda2ecb3133e6162b289
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-03-19 18:30:10 -0600

    Add files via upload
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

---
+++ tria-maaxboard-osm93/ota/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# ---------UN-COMMENT THIS COMMAND TO ENABLE SDK RE-INSTALLATION-------
+# python3 -m pip install --force-reinstall iotconnect-sdk-lite
+# ---------------------------------------------------------------------
+
+# This is the most basic form of install.sh where there are no files that need to be moved to additional directories. 
+# All files within the OTA package are extracted into /home/weston.
