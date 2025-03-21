54448207a26127eb4f1e0a60f8af465d874205ba
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-03-21 11:51:49 -0600

    Update device-setup.py
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- files/setup-files/device-setup.py
+++ files/setup-files/device-setup.py
@@ -5,53 +5,54 @@ import os
 import avnet.iotconnect.restapi.lib.template as template
 from avnet.iotconnect.restapi.lib.error import InvalidActionError
 from avnet.iotconnect.restapi.lib.template import TemplateCreateResult
+import avnet.iotconnect.restapi.lib.device as device
 '''
 # Get the current version of Python
 version = sys.version_info
 # Check if the major version is 3 and the minor version is at least 11
 if version.major != 3 or version.minor < 11:
-    print(f"Python version must be at least 3.11. Detected version is {version.major}.{version.minor}!")
+    print(f'Python version must be at least 3.11. Detected version is {version.major}.{version.minor}!')
     sys.exit(1)
 
 # Using pip to install or force reinstall the Lite SDK
 try:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "--force-reinstall", "iotconnect-sdk-lite"])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--force-reinstall', 'iotconnect-sdk-lite'])
 except subprocess.CalledProcessError as e:
-        print(f"Error occurred while installing the Lite SDK: {e}")
+        print(f'Error occurred while installing the Lite SDK: {e}')
         sys.exit(1)
   
 # Install API
 try:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "iotconnect-rest-api"])
-    print("iotconnect-rest-api has been successfully installed.")
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'iotconnect-rest-api'])
+    print('iotconnect-rest-api has been successfully installed.')
 except subprocess.CalledProcessError as e:
-    print(f"Error occurred while installing iotconnect-rest-api: {e}")
+    print(f'Error occurred while installing iotconnect-rest-api: {e}')
     sys.exit(1)
   
 # Get user credentials
-print("To use the IoTConnect API, you will need to enter your credentials. These will be stored for 24 hours and then deleted from memory for security.") 
-email = input("Enter your IOTC login email address: ")
-password = input("Enter your IOTC login password: ")
-solutionkey = input("Enter your IOTC solution key (if you don't know your solution key, you can request it via a support ticket on the IoTConnect online platform): ")
-platform = input("Enter your IOTC platform (az for Azure or aws for AWS): ")
-environment = input("Enter your IOTC environment (can be found in the 'Key Vault' of the IoTConnect online platform): ")
+print('To use the IoTConnect API, you will need to enter your credentials. These will be stored for 24 hours and then deleted from memory for security.') 
+email = input('Enter your IOTC login email address: ')
+password = input('Enter your IOTC login password: ')
+solutionkey = input('Enter your IOTC solution key (if you don't know your solution key, you can request it via a support ticket on the IoTConnect online platform): ')
+platform = input('Enter your IOTC platform (az for Azure or aws for AWS): ')
+environment = input('Enter your IOTC environment (can be found in the Key Vault of the IoTConnect online platform): ')
 command = f'iotconnect-cli configure -u {email} -p "{password}" --pf {platform} --env {environment} --skey={solutionkey}'
 try:
     subprocess.check_call(command, shell=True)
 except subprocess.CalledProcessError as e:
-    print(f"IoTConnect Credentials Error": {e}")
+    print(f'IoTConnect Credentials Error: {e}')
     sys.exit(1)
 
 # Generate certificate/key
-subj = "/C=US/ST=IL/L=Chicago/O=IoTConnect/CN=device"
+subj = '/C=US/ST=IL/L=Chicago/O=IoTConnect/CN=device'
 days = 36500  # 100 years
-ec_curve = "prime256v1"
+ec_curve = 'prime256v1'
 try:
-    subprocess.check_call(["openssl", "ecparam", "-name", ec_curve, "-genkey", "-noout", "-out", "device-pkey.pem"])
-    subprocess.check_call(["openssl", "req", "-new", "-days", str(days), "-nodes", "-x509", "-subj", subj, "-key", "device-pkey.pem", "-out", "device-cert.pem"])
-        print("X509 credentials are now generated as device-cert.pem and device-pkey.pem.")
+    subprocess.check_call(['openssl', 'ecparam', '-name', ec_curve, '-genkey', '-noout', '-out', '../core-files/device-pkey.pem'])
+    subprocess.check_call(['openssl', 'req', '-new', '-days', str(days), '-nodes', '-x509', '-subj', subj, '-key', '../core-files/device-pkey.pem', '-out', '../core-files/device-cert.pem'])
+        print('X509 credentials are now generated as device-cert.pem and device-pkey.pem.')
 except subprocess.CalledProcessError as e:
-    print(f"An error occurred while generating certificates: {e}", file=sys.stderr)
+    print(f'An error occurred while generating certificates: {e}', file=sys.stderr)
     sys.exit(1)
 '''
 
@@ -60,18 +61,21 @@ t = template.get_by_template_code('plitedemo')
 if t is not None:
     print("plitedemo template already exists in IOTC instance, skipping to device creation.")
 else:
-    print("plitedemo template not detected in IOTC instance. Adding it now...")
-    create_result: TemplateCreateResult = template.create('plitedemo_template.JSON', new_template_code="plitedemo", new_template_name="plitedemo")
+    print('plitedemo template not detected in IOTC instance. Adding it now...')
+    create_result: TemplateCreateResult = template.create('plitedemo_template.JSON', new_template_code='plitedemo', new_template_name='plitedemo')
     if create_result is None:
         raise Exception("Expected successful template creation")
 
 # Create IOTC Device
-with open('device-cert.pem', 'r') as file:
+with open('../core-files/device-cert.pem', 'r') as file:
     certificate = file.read()
     result = device.create(template_guid=t.guid, duid=DUID, device_certificate=certificate)
     print('create=', result)
 
 
-# get device config
+# Create device config
+device_config = config.generate_device_json('AAATESTDEVICE')
+with open('../core-files/iotcDeviceConfig.json', 'w') as f:
+    f.write(device_config)
 
-# download app.py
+# Download app.py
