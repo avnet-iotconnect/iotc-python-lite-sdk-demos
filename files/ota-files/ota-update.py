8f1109e45441fbac0d03ca25d6318c236ee9950a
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-03-24 12:57:50 -0600

    Update ota-update.py
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- files/ota-files/ota-update.py
+++ files/ota-files/ota-update.py
@@ -1,11 +1,15 @@
 import tarfile
 import os
 import shutil
+import sys
 import avnet.iotconnect.restapi.lib.template as template
 from avnet.iotconnect.restapi.lib import firmware, upgrade, device, config, ota, apiurl, util
 from avnet.iotconnect.restapi.lib.error import InvalidActionError, ConflictResponseError
 from avnet.iotconnect.restapi.lib.apirequest import request
+import avnet.iotconnect.restapi.lib.credentials as credentials
 from http import HTTPMethod, HTTPStatus
+import subprocess
+from getpass import getpass
 
 def create_ota_payload():
     # Get the current directory (the directory where the script is located)
@@ -41,6 +45,79 @@ def create_ota_payload():
         # Add the install.sh file
         if os.path.exists(install_sh):
             tar.add(install_sh, arcname='install.sh')
+
+def configure_creds():
+    while True:
+        try:
+            email = input('Enter your IOTC login email address: ')
+            psswd = getpass('Enter your IOTC login password: ')
+            solutionkey = input('Enter your IOTC solution key (if you do not know your solution key, you can request it via a support ticket on the IoTConnect online platform): ')
+            platform = input('Enter your IOTC platform (az for Azure or aws for AWS): ')
+            environment = input('Enter your IOTC environment (can be found in the Key Vault of the IoTConnect online platform): ')
+            config.env = environment
+            config.pf = platform
+            config.skey = solutionkey
+            apiurl.configure_using_discovery()
+            credentials.authenticate(username=email, password=psswd)
+            break
+        except Exception as e:
+            print(f'Error processing credentials, please try again: {e}')
+
+# Get the current version of Python
+version = sys.version_info
+# Check if the major version is 3 and the minor version is at least 11
+if version.major != 3 or version.minor < 11:
+    print(f'Python version must be at least 3.11. Detected version is {version.major}.{version.minor}!')
+    sys.exit(1)
+
+# Check list of installed packages
+result = subprocess.check_output([sys.executable, "-m", "pip", "list"], stderr=subprocess.PIPE)
+installed_packages = result.decode('utf-8')
+
+# Using pip to install or force reinstall the API
+install = True
+if 'iotconnect-rest-api' in installed_packages.lower():
+    while True:
+        y_or_n = input('iotconnect-rest-api is already installed. Would you like to force-reinstall it with the newest available version? (answer with y/Y/n/N)')
+        if y_or_n in ['y', 'Y']:
+            break
+        elif y_or_n in ['n', 'N']:
+            install = False
+            break
+        else:
+            print('Invalid response, please only use y or Y for yes and n or N for No.')
+if install == True:
+    try:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--force-reinstall', 'iotconnect-rest-api'])
+        print('iotconnect-rest-api has been successfully installed.')
+    except subprocess.CalledProcessError as e:
+        print(f'Error occurred while installing iotconnect-rest-api: {e}')
+        sys.exit(1)
+
+# Check login status and get user credentials if logged out
+logged_in = True
+if config.access_token is None:
+        logged_in = False
+else:
+    if config.token_expiry < datetime.datetime.now(datetime.timezone.utc).timestamp():
+        logged_in = False
+    elif config.token_time + 3600 < datetime.datetime.now(datetime.timezone.utc).timestamp() and os.environ.get('IOTC_API_NO_TOKEN_REFRESH') is None:
+        # It's been longer than an hour since we refreshed the token. We should refresh it now.
+        data = {
+        "refreshtoken": config.refresh_token
+        }
+        response = request(apiurl.ep_auth, "/Auth/refresh-token", json=data, headers={})
+        config.access_token = response.body.get_object_value("access_token")
+        config.refresh_token = response.body.get_object_value("refresh_token")
+        expires_in = response.body.get_object_value("expires_in")
+        config.token_time = datetime.datetime.now(datetime.timezone.utc).timestamp()
+        config.token_expiry = config.token_time + expires_in
+        config.write()
+if logged_in == True:
+    print('Already logged into IoTConnect on this device.')
+else:    
+    print('To use the IoTConnect API, you will need to enter your credentials. These will be stored for 24 hours and then deleted from memory for security.') 
+    configure_creds()
 
 create_ota_payload()
 
