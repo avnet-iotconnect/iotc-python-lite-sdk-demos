3367c51cf50681367c28b777fe86f180c3c2a1e2
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-03-26 15:22:27 -0600

    Update device-setup.py
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- files/setup-files/device-setup.py
+++ files/setup-files/device-setup.py
@@ -1,14 +1,27 @@
 import sys
 import subprocess
-import json
-import os
 import urllib.request
 import re
-import argparse
-import datetime
+import avnet.iotconnect.restapi.lib.template as template
+from avnet.iotconnect.restapi.lib import device, config
+
+'''
+Assumptions before running this script:
+1) The IoTConnect REST API is already installed on this device
+2) The IoTConnect Python Lite SDK is already installed on this device
+3) The user is currently logged into their IoTConnect account on this machine via the REST API CLI
 
 
-def get_device_id():
+Script Flow:
+1) A new certificate and private key are generated for this device
+2) If the "plitedemo" template does not already exist for this IOTC entity, it is created
+3) The user inputs their desired unique ID for the device and it is created
+4) A config file is generated for the device
+5) The basic starter IOTC python program is downloaded to this device
+'''
+
+# Prompts user for a unique device ID and verifies that it is a valid entry
+def get_input_device_id():
     while True:
         device_id = input('Enter a unique ID for your device (only alphanumeric chars and non-endcap hyphens allowed):')
         if re.match(r'^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$', device_id):
@@ -16,111 +29,44 @@ def get_device_id():
         print('The unique ID can only include alphanumeric characters and non-endcap hyphens. Please try again.')
 
 
-# Get the current version of Python
-version = sys.version_info
-# Check if the major version is 3 and the minor version is at least 11
-if version.major != 3 or version.minor < 11:
-    print(f'Python version must be at least 3.11. Detected version is {version.major}.{version.minor}!')
-    sys.exit(1)
-# Check list of installed packages
-result = subprocess.check_output([sys.executable, "-m", "pip", "list"], stderr=subprocess.PIPE)
-installed_packages = result.decode('utf-8')
-# Remove python package that has dependencies that conflict with IoTConnect libraries
-if 'azure-iot-device' in installed_packages.lower():
-    try:
-        subprocess.check_call([sys.executable, '-m', 'pip', 'uninstall', '-y', 'azure-iot-device'])
-    except subprocess.CalledProcessError as e:
-        print(f'Error occurred while uninstalling azure-iot-device: {e}')
-# Using pip to install or force reinstall the Lite SDK
-install = True
-if 'iotconnect-sdk-lite' in installed_packages.lower():
-    while True:
-        y_or_n = input('iotconnect-sdk-lite is already installed. Would you like to force-reinstall it with the newest available version? (answer with y/Y/n/N)')
-        if y_or_n in ['y', 'Y']:
-            break
-        elif y_or_n in ['n', 'N']:
-            install = False
-            break
-        else:
-            print('Invalid response, please only use y or Y for yes and n or N for No.')
-if install == True:
-    try:
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--force-reinstall', 'iotconnect-sdk-lite'])
-    except subprocess.CalledProcessError as e:
-        print(f'Error occurred while installing the Lite SDK: {e}')
-        sys.exit(1)
+#----------------MAIN---------------------
 
-
-  
-# Using pip to install or force reinstall the API
-install = True
-if 'iotconnect-rest-api' in installed_packages.lower():
-    while True:
-        y_or_n = input('iotconnect-rest-api is already installed. Would you like to force-reinstall it with the newest available version? (answer with y/Y/n/N)')
-        if y_or_n in ['y', 'Y']:
-            break
-        elif y_or_n in ['n', 'N']:
-            install = False
-            break
-        else:
-            print('Invalid response, please only use y or Y for yes and n or N for No.')
-if install == True:
-    try:
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--force-reinstall', 'iotconnect-rest-api'])
-        print('iotconnect-rest-api has been successfully installed.')
-    except subprocess.CalledProcessError as e:
-        print(f'Error occurred while installing iotconnect-rest-api: {e}')
-        sys.exit(1)
-
-import avnet.iotconnect.restapi.lib.template as template
-from avnet.iotconnect.restapi.lib.error import InvalidActionError
-from avnet.iotconnect.restapi.lib.template import TemplateCreateResult
-from avnet.iotconnect.restapi.lib import device, config
-import avnet.iotconnect.restapi.lib.credentials as credentials
-import avnet.iotconnect.restapi.lib.apiurl as apiurl
-from avnet.iotconnect.restapi.lib.apirequest import Headers, request
-
-# Generate certificate/key
+# Generate certificate and private key for device
 subj = '/C=US/ST=IL/L=Chicago/O=IoTConnect/CN=device'
 days = 36500  # 100 years
 ec_curve = 'prime256v1'
-try:
-    subprocess.check_call(['openssl', 'ecparam', '-name', ec_curve, '-genkey', '-noout', '-out', 'device-pkey.pem'])
-    subprocess.check_call(['openssl', 'req', '-new', '-days', str(days), '-nodes', '-x509', '-subj', subj, '-key', 'device-pkey.pem', '-out', 'device-cert.pem'])
-    print('X509 credentials are now generated as device-cert.pem and device-pkey.pem.')
-except subprocess.CalledProcessError as e:
-    print(f'An error occurred while generating certificates: {e}', file=sys.stderr)
-    sys.exit(1)
+subprocess.check_call(['openssl', 'ecparam', '-name', ec_curve, '-genkey', '-noout', '-out', 'device-pkey.pem'])
+subprocess.check_call(['openssl', 'req', '-new', '-days', str(days), '-nodes', '-x509', '-subj', subj, '-key', 'device-pkey.pem', '-out', 'device-cert.pem'])
+print('X509 credentials are now generated as device-cert.pem and device-pkey.pem.')
 
-# Create IOTC template
+# Create plitedemo template if it does not exist in this entity
 t = template.get_by_template_code('plitedemo')
-if t is not None:
-    print('plitedemo template already exists in IOTC instance, skipping to device creation.')
-else:
+if t is None:
     print('plitedemo template not detected in IOTC instance. Adding it now...')
-    create_result: TemplateCreateResult = template.create('plitedemo_template.JSON', new_template_code='plitedemo', new_template_name='plitedemo')
-    if create_result is None:
-        raise Exception("Expected successful template creation")
+    create_result = template.create('plitedemo_template.JSON', new_template_code='plitedemo', new_template_name='plitedemo')
 
 # Create IOTC Device
 with open('device-cert.pem', 'r') as file:
+    # Previously-generated certificate is used in device creation
     certificate = file.read()
+    # Loop breaks once device is successfully created
     while True:
-        device_id = get_device_id()
+        # User inputs a valid unique ID
+        device_id = get_input_device_id()
         try:
+            # Attempt device creation
             result = device.create(template_guid=t.guid, duid=device_id, device_certificate=certificate)
+            # If no exceptions, exit loop
             break
+        # Most common exception will be for the given unique ID already being in use
         except Exception as e:
             print(f"An exception occurred while attempting to create the device, please try again.: {e}")
 
-# Create device config
+# Create device config file
 device_config = config.generate_device_json(device_id)
 with open('iotcDeviceConfig.json', 'w') as f:
     f.write(device_config)
 
 # Download app.py
-try:
-    urllib.request.urlretrieve('https://raw.githubusercontent.com/avnet-iotconnect/iotc-python-lite-sdk-demos/refs/heads/main/files/core-files/app.py', 'app.py')
-    print('app.py successfully downloaded. Run it with "python3 app.py".')
-except Exception as e:
-    print(f'Error downloading app file: {e}')
+urllib.request.urlretrieve('https://raw.githubusercontent.com/avnet-iotconnect/iotc-python-lite-sdk-demos/refs/heads/main/files/core-files/app.py', 'app.py')
+print('app.py successfully downloaded. Run it with "python3 app.py".')
