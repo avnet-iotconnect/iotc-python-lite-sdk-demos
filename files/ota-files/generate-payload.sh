914696197a4717d143e7f2a1efeec10acc5f5f7d
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-03-31 14:47:57 -0600

    Update generate-payload.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- files/ota-files/generate-payload.sh
+++ files/ota-files/generate-payload.sh
@@ -7,15 +7,17 @@ script_dir=$(dirname "$(realpath "$0")")
 core_files_dir="$script_dir/../core-files"
 additional_files_dir="$script_dir/../additional-files"
 install_sh="$script_dir/install.sh"
+device_replacement_py="$script_dir/device-replacement.py"  # Add device-replacement.py explicitly
 
 # Define the tar.gz output file name
 output_tar_gz="$script_dir/ota-payload.tar.gz"
 
 # Create the .tar.gz file
-tar -czf "$output_tar_gz" -C "$script_dir" \
+tar -czf "$output_tar_gz" -C "$core_files_dir" \
     $(find "$core_files_dir" -type f -exec basename {} \;) \
+    -C "$additional_files_dir" \
     $(find "$additional_files_dir" -type f ! -name "placeholder.txt" -exec basename {} \;) \
-    install.sh \
-    device-replacement.py
+    -C "$script_dir" \
+    install.sh
 
 echo "OTA payload created successfully at $output_tar_gz"
