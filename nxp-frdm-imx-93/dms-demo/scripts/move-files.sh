f9cb9799ea6a87ea43187574837428df784aaa1e
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-03-31 14:29:44 -0600

    Update and rename generate-payload.sh to move-files.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

---
+++ nxp-frdm-imx-93/dms-demo/scripts/move-files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get the current directory (the directory where the script is located)
+script_dir=$(dirname "$(realpath "$0")")
+
+# Define source and destination directories
+source_dir="$script_dir/.."
+core_files_dir="$script_dir/../../../files/core-files"
+additional_files_dir="$script_dir/../../../files/additional-files"
+
+# Define the files to be copied
+dms_processing_file="$source_dir/dms-processing.py"
+app_file="$source_dir/app.py"
+download_models_file="$source_dir/download_models.py"
+eiqIOTC_template_file="$source_dir/templates/eiqIOTC_template.JSON"
+
+# Copy files to the target directories
+echo "Copying files to $core_files_dir..."
+cp "$dms_processing_file" "$core_files_dir"
+cp "$app_file" "$core_files_dir"
+cp "$download_models_file" "$core_files_dir"
+
+echo "Copying eiqIOTC_template.JSON to $additional_files_dir..."
+cp "$eiqIOTC_template_file" "$additional_files_dir"
+
+echo "Files copied successfully."
