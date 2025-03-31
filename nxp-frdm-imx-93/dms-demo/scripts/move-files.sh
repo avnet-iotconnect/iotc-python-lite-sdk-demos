ea9a6e982b1501e53d35fe2894c176c61f277f17
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-03-31 14:36:29 -0600

    Update move-files.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- nxp-frdm-imx-93/dms-demo/scripts/move-files.sh
+++ nxp-frdm-imx-93/dms-demo/scripts/move-files.sh
@@ -7,12 +7,14 @@ script_dir=$(dirname "$(realpath "$0")")
 source_dir="$script_dir/.."
 core_files_dir="$script_dir/../../../files/core-files"
 additional_files_dir="$script_dir/../../../files/additional-files"
+ota_files_dir="$script_dir/../../../files/ota-files"
 
 # Define the files to be copied
 dms_processing_file="$source_dir/dms-processing.py"
 app_file="$source_dir/app.py"
 download_models_file="$source_dir/download_models.py"
 eiqIOTC_template_file="$source_dir/templates/eiqIOTC_template.JSON"
+install_sh_file="$script_dir/install.sh"
 
 # Copy files to the target directories
 echo "Copying files to $core_files_dir..."
@@ -22,5 +24,8 @@ cp "$download_models_file" "$core_files_dir"
 
 echo "Copying eiqIOTC_template.JSON to $additional_files_dir..."
 cp "$eiqIOTC_template_file" "$additional_files_dir"
+
+echo "Copying install.sh to $ota_files_dir..."
+cp "$install_sh_file" "$ota_files_dir"
 
 echo "Files copied successfully."
