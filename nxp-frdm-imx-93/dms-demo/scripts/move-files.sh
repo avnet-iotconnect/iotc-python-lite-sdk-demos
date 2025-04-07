7bf3303f351f46ecc0ac4de7570b82179c5361bb
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-04-06 22:13:51 -0600

    Update move-files.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- nxp-frdm-imx-93/dms-demo/scripts/move-files.sh
+++ nxp-frdm-imx-93/dms-demo/scripts/move-files.sh
@@ -4,7 +4,7 @@
 script_dir=$(dirname "$(realpath "$0")")
 
 # Define source and destination directories
-source_dir="$script_dir/.."
+source_dir="$script_dir/../python-files"
 core_files_dir="$script_dir/../../../files/core-files"
 additional_files_dir="$script_dir/../../../files/additional-files"
 ota_files_dir="$script_dir/../../../files/ota-files"
@@ -13,7 +13,7 @@ ota_files_dir="$script_dir/../../../files/ota-files"
 dms_processing_file="$source_dir/dms-processing.py"
 app_file="$source_dir/app.py"
 download_models_file="$source_dir/download_models.py"
-eiqIOTC_template_file="$source_dir/templates/eiqIOTC_template.JSON"
+eiqIOTC_template_file="$source_dir/../templates/eiqIOTC_template.JSON"
 install_sh_file="$script_dir/install.sh"
 
 # Copy files to the target directories
