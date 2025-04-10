bb8d10bd8c51d74208a5f1b201ef418a27c6db22
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-04-10 15:15:05 -0600

    Update app.py
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- files/core-files/app.py
+++ files/core-files/app.py
@@ -1,40 +1,73 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2024 Avnet
-# Authors: Nikola Markovic <nikola.markovic@avnet.com> and Zackary Andraka <zackary.andraka@avnet.com> et al.
+# Authors: Nikola Markovic <nikola.markovic@avnet.com> et al.
 # This is a self-updating app with OTA support that can update itself with a new OTA package.
 
 import random
 import sys
 import time
+from dataclasses import dataclass
 import subprocess
 import os
 import urllib.request
-
+import requests
 
 from avnet.iotconnect.sdk.lite import Client, DeviceConfig, C2dCommand, Callbacks, DeviceConfigError
 from avnet.iotconnect.sdk.lite import __version__ as SDK_VERSION
 from avnet.iotconnect.sdk.sdklib.mqtt import C2dAck, C2dOta
 
-"""
-See minimal.py example for a way to configure the device without the JSON file
-You can download the iotcDeviceConfig.json by clicking the cog icon in the upper right of your device's info panel
-NOTE: If you do not pass the server certificate, we will use the system's trusted certificate store, if available.
-For example, the trusted Root CA certificates from the in /etc/ssl/certs will be used on Linux.
-However, it is more secure to pass the actual server CA Root certificate in order to avoid potential MITM attacks.
-On Linux, you can use server_ca_cert_path="/etc/ssl/certs/DigiCert_Global_Root_CA.pem" for Azure
-or server_ca_cert_path="/etc/ssl/certs/Amazon_Root_CA_1.pem" for AWS
-"""
+
+def extract_and_run_tar_gz(targz_filename: str):
+    try:
+        subprocess.run(("tar", "-xzvf", targz_filename, "--overwrite"), check=True)
+        current_directory = os.getcwd()
+        script_file_path = os.path.join(current_directory, "install.sh")
+        # If install.sh is found in the current directory, execute it
+        if os.path.isfile(script_file_path):
+            try:
+                subprocess.run(['bash', script_file_path], check=True)
+                print(f"Successfully executed install.sh")
+                return True
+            except subprocess.CalledProcessError as e:
+                print(f"Error executing install.sh: {e}")
+                return False
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                return False
+        else:
+            print("install.sh not found in the current directory.")
+            return False
+    except subprocess.CalledProcessError:
+        return False
+
 
 def on_command(msg: C2dCommand):
+    global c
     print("Received command", msg.command_name, msg.command_args, msg.ack_id)
-    if msg.command_name == "set-user-led":
-        if len(msg.command_args) == 3:
-            status_message = "Setting User LED to R:%d G:%d B:%d" % (int(msg.command_args[0]), int(msg.command_args[1]), int(msg.command_args[2]))
+    if msg.command_name == "file-download":
+        if len(msg.command_args) == 1:
+            status_message = "Downloading %s to device" % (msg.command_args[0])
+            response = requests.get(msg.command_args[0])
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                # Open the file in binary write mode and save the content
+                with open('ota-payload.tar.gz', 'wb') as file:
+                    for chunk in response.iter_content(chunk_size=8192): 
+                        file.write(chunk)
+                print(f"File downloaded successfully and saved to ota-payload.tar.gz")
+            else:
+                print(f"Failed to download the file. Status code: {response.status_code}")
             c.send_command_ack(msg, C2dAck.CMD_SUCCESS_WITH_ACK, status_message)
             print(status_message)
+            extraction_success = extract_and_run_tar_gz('ota-payload.tar.gz')
+            print("Download command successful. Will restart the application...")
+            print("")  # Print a blank line so it doesn't look as confusing in the output.
+            sys.stdout.flush()
+            # restart the process
+            os.execv(sys.executable, [sys.executable, __file__] + [sys.argv[0]])
         else:
-            c.send_command_ack(msg, C2dAck.CMD_FAILED, "Expected 3 arguments")
-            print("Expected three command arguments, but got", len(msg.command_args))
+            c.send_command_ack(msg, C2dAck.CMD_FAILED, "Expected 1 argument")
+            print("Expected 1 command argument, but got", len(msg.command_args))	
     else:
         print("Command %s not implemented!" % msg.command_name)
         if msg.ack_id is not None: # it could be a command without "Acknowledgement Required" flag in the device template
@@ -42,9 +75,10 @@ def on_command(msg: C2dCommand):
 
 
 def on_ota(msg: C2dOta):
+    global c
     print("Starting OTA downloads for version %s" % msg.version)
-    error_msg = None
     c.send_ota_ack(msg, C2dAck.OTA_DOWNLOADING)
+    extraction_success = False
     for url in msg.urls:
         print("Downloading OTA file %s from %s" % (url.file_name, url.url))
         try:
@@ -53,42 +87,21 @@ def on_ota(msg: C2dOta):
             print("Encountered download error", e)
             error_msg = "Download error for %s" % url.file_name
             break
-        try:
-            if url.file_name.endswith(".tar.gz"):
-                subprocess.run(("tar", "-xzvf", url.file_name, "--overwrite"), check=True)
-                filename = "install.sh"
-                current_directory = os.getcwd()
-                file_path = os.path.join(current_directory, filename)
-                # If ota-install.sh is found in the current directory, give it executable permissions and execute it
-                if os.path.isfile(file_path):
-                    try:
-                        subprocess.run(['bash', file_path], check=True)
-                        print(f"Successfully executed {filename}")
-
-                    except subprocess.CalledProcessError as e:
-                        print(f"Error executing {filename}: {e}")
-                    except Exception as e:
-                        print(f"An error occurred: {e}")
-                else:
-                    print(f"{filename} not found in the current directory.")
-            else:
-                print("ERROR: Unhandled file format for file %s" % url.file_name)
-                error_msg = "Processing error for %s" % url.file_name
+        if url.file_name.endswith(".tar.gz"):
+            extraction_success = extract_and_run_tar_gz(url.file_name)
+            if extraction_success is False:
                 break
-        except subprocess.CalledProcessError:
-            print("ERROR: Failed to install %s" % url.file_name)
-            error_msg = "Install error for %s" % url.file_name
-            break
-    if error_msg is not None:
-        c.send_ota_ack(msg, C2dAck.OTA_FAILED, error_msg)
-        print('Encountered a download processing error "%s". Not restarting.' % error_msg)  # In hopes that someone pushes a better update
-    else:
+        else:
+            print("ERROR: Unhandled file format for file %s" % url.file_name)
+    if extraction_success is True:
         print("OTA successful. Will restart the application...")
         c.send_ota_ack(msg, C2dAck.OTA_DOWNLOAD_DONE)
         print("")  # Print a blank line so it doesn't look as confusing in the output.
         sys.stdout.flush()
         # restart the process
         os.execv(sys.executable, [sys.executable, __file__] + [sys.argv[0]])
+    else:
+        print('Encountered a download processing error. Not restarting.')
 
 
 def on_disconnect(reason: str, disconnected_from_server: bool):
