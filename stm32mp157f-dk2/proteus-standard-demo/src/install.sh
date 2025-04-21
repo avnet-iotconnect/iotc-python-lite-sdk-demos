db3787941fae9b8803e72c46e341dfb05bce211f
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-04-21 13:11:07 -0600

    Update install.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- stm32mp157f-dk2/proteus-standard-demo/src/install.sh
+++ stm32mp157f-dk2/proteus-standard-demo/src/install.sh
@@ -7,43 +7,11 @@
 # python3 -m pip install --force-reinstall iotconnect-sdk-lite
 # ---------------------------------------------------------------------
 
-if true; then
-
-export PIP_ROOT_USER_ACTION=ignore  # Suppresses venv warning
-
-PIP_ROOT_USER_ACTION=ignore python3 -m pip install flask numpy opencv-python requests filelock networkx
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout key.pem -out cert.pem -subj "/CN=localhost"
-
-cp ./download_models.py /usr/bin/eiq-examples-git
-
-cd /usr/bin/eiq-examples-git
-
-python3 download_models.py
-
-cd /home/weston/demo
-
-# Create empty dms-data.json with read/write perms
-touch dms-data.json
-chmod 666 dms-data.json
-
-cp /usr/bin/eiq-examples-git/dms/face_detection.py .
-cp /usr/bin/eiq-examples-git/dms/eye_landmark.py .
-cp /usr/bin/eiq-examples-git/dms/face_landmark.py .
-cp /usr/bin/eiq-examples-git/dms/utils.py .
-
-board_ip=$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++){if($i=="src"){print $(i+1); exit}}}')
-echo "Camera Live Stream url: https://$board_ip:8080/live"
-
-fi
-
-# Find and copy all .tflite files in the current directory to the models directory
-DEST_DIR="/usr/bin/eiq-examples-git/models"
-for file in *.tflite; do
-  if [ -f "$file" ]; then
-    echo "Moving $file to $DEST_DIR"
-    cp "$file" "$DEST_DIR"
-  fi
-done
+dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "Installing bleak python library, may take several minutes to complete."
+pip3 install bleak
 
 echo "Installation complete!"
