86514eb1a54df686606a2e86ee071b0056a4967b
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-05-29 12:25:01 -0600

    Update launch-vision-program.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- stm32mp135f-dk/ai-vision/src/launch-vision-program.sh
+++ stm32mp135f-dk/ai-vision/src/launch-vision-program.sh
@@ -2,4 +2,4 @@
 
 source /usr/local/x-linux-ai/resources/config_board_cpu.sh
 cmd="python3 /usr/local/x-linux-ai/object-detection/iotc-vision-program.py -m /usr/local/x-linux-ai/object-detection/models/$OBJ_DETEC_MODEL -l /usr/local/x-linux-ai/object-detection/models/$OBJ_DETEC_MODEL_LABEL.txt --framerate $DFPS --frame_width $DWIDTH --conf_threshold $1 --frame_height $DHEIGHT $OPTIONS"
-eval $cmd
+su -l weston -c "$cmd"
